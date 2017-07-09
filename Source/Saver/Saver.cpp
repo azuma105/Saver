b8cbd5d5c82f4ec10f397729d3fdb05a9ce3b6e7
azuma105 <notorious_ton@hotmail.co.jp>
2017-07-09 23:19:46 +0900

    初期ソース＆設定ファイル追加

---
+++ Source/Saver/Saver.cpp
@@ -0,0 +1,7 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#include "Saver.h"
+
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Saver, "Saver" );
+ 
---
+++ Source/Saver/SaverGameMode.cpp
@@ -0,0 +1,15 @@
+// Copyright 1998-2017 Epic Games, Inc. All Rights Reserved.
+
+#include "Saver.h"
+#include "SaverGameMode.h"
+#include "SaverCharacter.h"
+
+ASaverGameMode::ASaverGameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+}
